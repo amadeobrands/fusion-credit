011a120026d3bbee415b9493bf6ca3081ce7fa28
Ted Wu <ted.wu@theguarantors.com>
2022-06-25 18:40:01 -0400

    Add retrieve score from contract

--- pages/score.js
+++ pages/score.js
@@ -3,15 +3,24 @@ import React from "react"
 import { useWeb3React } from "@web3-react/core"
 import styles from "../styles/Home.module.css"
 import EthAccount from "../components/EthAccount"
+import { Contract, utils } from "ethers"
+import FusionCredit from "../artifacts/contracts/FusionCredit.sol/FusionCredit.json"
 
 export default function Score() {
   const web3React = useWeb3React()
   const [score, setScore] = React.useState({score: null, version: null, timestamp: null})
 
-  async function getScore() {
-    // Get score from blockchain
+  async function getScore(event) {
+    event.preventDefault()
 
-    setScore({score: 600, version: 1, timestamp: 1656167800})
+    const address = utils.getAddress(event.target.address.value)
+    console.log("Getting score for address: " + address)
+
+    const contractAddress = "0x570873f17e7c7b7736d79f357D36299ca2d13311"
+    const contract = new Contract(contractAddress, FusionCredit.abi, web3React.library)
+
+    const res = await contract.getScore(address)
+    setScore({score: parseInt(res.score), version: parseInt(res.version), timestamp: parseInt(res.timestamp)})
   }
 
   return (
@@ -42,7 +51,7 @@ export default function Score() {
           { score.score === null && 
             <form className={styles.formctn} onSubmit={getScore}>
               <div>
-                Address: <input className={styles.formfield} placeholder="Enter Address" />
+                Address: <input name="address" className={styles.formfield} placeholder="Enter Address" />
               </div>
               <div className={styles.formbuttonctn}>
                 <input className={styles.button} type="submit" value="Get Fusion Score" />
