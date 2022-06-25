7927fc58333118bc757ebbc072907bbd86a5c63c
Ted Wu <ted.wu@theguarantors.com>
2022-06-25 09:44:10 -0400

    UI Implementation

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
 import '../styles/globals.css'
+import { Web3ReactProvider } from "@web3-react/core"
+import { ethers } from "ethers"
+
+const getLibrary = (provider) => {
+  const library = new ethers.providers.Web3Provider(provider)
+  library.pollingInterval = 2000
+  return library
+}
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Web3ReactProvider getLibrary={getLibrary}>
+    <Component {...pageProps} />
+  </Web3ReactProvider>
 }
 
 export default MyApp
