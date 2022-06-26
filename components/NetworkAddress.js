e0c684c607ea4cc48c7b8d1069e6edddd9acb052
Ted Wu <ted.wu@theguarantors.com>
2022-06-25 20:48:40 -0400

    Add current account indication

--- components/NetworkAddress.js
+++ components/NetworkAddress.js
@@ -1,10 +1,20 @@
 import React from "react"
 import styles from "../styles/Home.module.css"
+import { useWeb3React } from "@web3-react/core"
 
 const NetworkAddress = ({ account, onDelete }) => {
+  const web3React = useWeb3React()
+
+  console.log(account)
+  console.log(web3React)
+
   return <div className={styles.card}>
     <p>Network: {account.network}</p>
     <p>Address: {account.address}</p>
+
+    { web3React.chainId == account.chainId && web3React.account == account.address &&
+      <p className={styles.accounttgt}>Fusion Score will be attached to this address</p>
+    }
   </div>
 }
 
